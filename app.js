7be9cb5ca2b620bd4314666466c049578bfe1128
James Tang <jytang10@gmail.com>
2019-11-13 16:15:30 -0800

    Fixed stroke white-black color change issue

--- app.js
+++ app.js
@@ -95,11 +95,17 @@ function init() {
 
   const tl = new TimelineMax({paused: true, reversed: true});
 
-  tl.to(navOpen, 0.5, {y:0})
-    .fromTo(contact, 0.5, {opacity:0, y:10}, {opacity:1, y:0}, '-=0.1')
-    .fromTo(social, 0.5, {opacity:0, y:10}, {opacity:1, y:0}, '-=-0.5')
-    .fromTo(logo, 0.2, {color:'white'}, {color: 'black'}, '-=1')
-    .from(hamburgerLines, 0.2, {stroke: 'white'}, {stroke: 'black'}, '-=1')
+  tl.to(navOpen, 0.5, { y: 0 })
+    .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.1")
+    .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.5")
+    .fromTo(logo, 0.2, { color: "white" }, { color: "black" }, "-=1")
+    .fromTo(
+      hamburgerLines,
+      0.2,
+      { stroke: "white" },
+      { stroke: "black" },
+      "-=1"
+    );
 
   hamburger.addEventListener('click', () => {
     tl.reversed() ? tl.play() : tl.reverse();
